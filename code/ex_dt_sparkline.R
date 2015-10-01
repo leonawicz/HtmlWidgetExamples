5bab8fab44ffb0ce672a16c507d26d90204452ac
Matt Leonawicz <matt.rstat@gmail.com>
2015-10-01 09:19:40 -0600

    Update script

--- code/ex_dt_sparkline.R
+++ code/ex_dt_sparkline.R
@@ -7,10 +7,10 @@ library(DT)
 library(sparkline)
 
 dat <- mutate(dat, Decade=paste0(Year - Year %% 10, "s"))
-r <- range(filter(dat, Var=="Temperature" & Location=="Edmonton, Alberta")$Val)
+r <- range(filter(dat, Var=="Temperature")$Val)
 
-# @knitr table_full
-datatable(dat, rownames=FALSE, filter="bottom")
+# @knitr table
+datatable(dat, rownames=FALSE)
 
 # @knitr defs
 colDefs1 <- list(list(targets=c(1:12), render=JS("function(data, type, full){ return '<span class=spark>' + data + '</span>' }")))
@@ -32,33 +32,33 @@ dat.p <- filter(dat, Var=="Precipitation" & Decade=="2000s" & Month=="Aug")$Val
 dat.p
 
 # @knitr sparkline_dt_prep
-dat.t <- filter(dat, Var=="Temperature" & Location=="Edmonton, Alberta") %>%
+dat.t <- filter(dat, Var=="Temperature") %>%
     group_by(Decade, Month) %>% summarise(Temperature=paste(Val, collapse = ","))
 dat.ta <- dcast(dat.t, Decade ~ Month)
 dat.tb <- dcast(dat.t, Month ~ Decade)
 
 # @knitr table_DxM_line
-d1 <- datatable(data.table(dat.ta), rownames=FALSE, filter="bottom", options=list(columnDefs=colDefs1, fnDrawCallback=cb_line))
+d1 <- datatable(data.table(dat.ta), rownames=FALSE, options=list(columnDefs=colDefs1, fnDrawCallback=cb_line))
 d1$dependencies <- append(d1$dependencies, htmlwidgets:::getDependency('sparkline'))
 d1
 
 # @knitr table_MxD_bar
-d2 <- datatable(data.table(dat.tb), rownames=FALSE, filter="bottom", options=list(columnDefs=colDefs2, fnDrawCallback=cb_bar))
+d2 <- datatable(data.table(dat.tb), rownames=FALSE, options=list(columnDefs=colDefs2, fnDrawCallback=cb_bar))
 d2$dependencies <- append(d2$dependencies, htmlwidgets:::getDependency('sparkline'))
 d2
 
 # @knitr table_MxD_box1
-d3 <- datatable(data.table(dat.tb), rownames=FALSE, filter="bottom", options=list(columnDefs=colDefs2, fnDrawCallback=cb_box1))
+d3 <- datatable(data.table(dat.tb), rownames=FALSE, options=list(columnDefs=colDefs2, fnDrawCallback=cb_box1))
 d3$dependencies <- append(d3$dependencies, htmlwidgets:::getDependency('sparkline'))
 d3
 
 # @knitr table_MxD_box2
-d4 <- datatable(data.table(dat.tb), rownames=FALSE, filter="bottom", options=list(columnDefs=colDefs2, fnDrawCallback=cb_box2))
+d4 <- datatable(data.table(dat.tb), rownames=FALSE, options=list(columnDefs=colDefs2, fnDrawCallback=cb_box2))
 d4$dependencies <- append(d4$dependencies, htmlwidgets:::getDependency('sparkline'))
 d4
 
 # @knitr final_prep
-dat.t2 <- filter(dat, Var=="Temperature" & Location=="Edmonton, Alberta" & Month=="Aug") %>%
+dat.t2 <- filter(dat, Var=="Temperature" & Month=="Aug") %>%
     group_by(Location, Month, Var, Decade) %>%
     summarise(Mean=round(mean(Val), 1), SD=round(sd(Val), 2), Min=min(Val), Max=max(Val), Samples=paste(Val, collapse = ",")) %>%
     mutate(Series=Samples) %>% data.table
@@ -72,6 +72,6 @@ cb = JS(paste0("function (oSettings, json) {
 }"), collapse="")
 
 # @knitr table_final
-d5 <- datatable(data.table(dat.t2), rownames=FALSE, filter="bottom", options=list(columnDefs=cd, fnDrawCallback=cb))
+d5 <- datatable(data.table(dat.t2), rownames=FALSE, options=list(columnDefs=cd, fnDrawCallback=cb))
 d5$dependencies <- append(d5$dependencies, htmlwidgets:::getDependency('sparkline'))
 d5
